de51f03c292e2696db3ec5939f9acb00f720e227
egorlitin <egorulitin@gmail.com>
2023-11-02 20:58:41 +0300

    add not found page

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -163,7 +163,6 @@ export async function fetchInvoiceById(id: string) {
 
     const invoice = data.rows.map((invoice) => ({
       ...invoice,
-      // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
 
