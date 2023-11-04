208e8e3136495e2f6c118cfa2b54edbee755d3a1
egorlitin <egorulitin@gmail.com>
2023-11-04 18:11:57 +0300

    add metadata to pages

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,11 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import Link from 'next/link';
 import { lusitana } from './ui/fonts';
 import Image from 'next/image';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
 
 export default function Page() {
   return (
